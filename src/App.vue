8c36cada0765d8ba6b12f5d34e090c0b79137434
liuyh <liuyh@163.com>
2019-11-29 14:50:00 +0800

    右键菜单

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <rightMenu />
+  </div>
+</template>
+
+<script>
+import rightMenu from './components/rightMenu.vue'
+
+export default {
+  name: 'app',
+  components: {
+    rightMenu
+  }
+}
+</script>
+
+<style>
+</style>
