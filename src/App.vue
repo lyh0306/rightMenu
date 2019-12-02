8dbcd1106202ea256e7f3923205a2a97034615ac
liuyh <liuyh@163.com>
2019-12-02 18:07:01 +0800

    新加按钮切换12.2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <rightMenu />
-    <tabButton />
+    <el-button v-for="(item, index) in btnList" :key="item.index" @click="handle(index)" :class="{isActive : handleClick === index}">{{item.name}}</el-button>
+    <el-divider content-position="left">我们是不一样的</el-divider>
+<!--    // 右键菜单-->
+    <rightMenu v-if="handleClick === 0" />
+<!--    点击切换-->
+    <tabButton v-if="handleClick === 1"/>
+    <!--    动画-->
+    <dhAnimate v-if="handleClick === 2"/>
 
   </div>
 </template>
@@ -9,12 +15,29 @@
 <script>
 import rightMenu from './components/rightMenu.vue'
 import tabButton from './components/tabButton.vue'
+import dhAnimate from './components/dhAnimate.vue'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      btnList: [
+        {name: '右键菜单'},
+        {name: '切换按钮'},
+        {name: '动画'},
+      ],
+      handleClick: 0
+    }
+  },
   components: {
     rightMenu,
-    tabButton
+    tabButton,
+    dhAnimate
+  },
+  methods: {
+    handle (index) {
+      this.handleClick = index
+    }
   }
 }
 </script>
---
+++ src/components/dhAnimate.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="dh">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "dhAnimate",
+        data () {
+            return {
+                msg: '动画'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
